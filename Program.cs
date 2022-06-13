327268cd5c9246ce5a61c14d0e23e11802afb2c5
Naran Gomes <gom3snaran@gmail.com>
2022-06-13 14:39:16 -0300

    Fabricas e o porblema de criacao de objetos

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using DesignPatterns2.Cap_1;
+using System.Data;
+using System.Data.SqlClient;
+
+IDbConnection conexao = new ConnectionFactory().GetConnection();
+
+IDbCommand comando = conexao.CreateCommand();
+comando.CommandText = "select * from tabela";
